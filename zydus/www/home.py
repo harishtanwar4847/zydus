5b4e758e46405be0d945fac55c1e3214e199dfa4
Neel Bhanushali <neel@atriina.com>
2022-03-19 23:08:55 +0000

    include file for my uploads entry | using context to populate my uploads on homepage

--- zydus/www/home.py
+++ zydus/www/home.py
@@ -9,3 +9,12 @@ def get_context(context):
         {"color": "#FF00FF", "logo": "/assets/zydus/images/company_logo.png", "title": "Digital Maketing - Facebook", "month_year": "Jan 2022", "number_of_files": 1},
         {"color": "#000000", "logo": "/assets/zydus/images/company_logo.png", "title": "Digital Maketing - Facebook", "month_year": "Jan 2022", "number_of_files": 1}, 
     ]
+
+    context['my_uploads'] = [
+        {"color": "#4F9DD9", "brand": "Everyuth", "title": "Digital Maketing - Facebook", "month_year": "Jan 2022", "number_of_files": 1},
+        {"color": "#FF0000", "brand": "Everyuth", "title": "Digital Maketing - Facebook", "month_year": "Jan 2022", "number_of_files": 2},
+        {"color": "#00FF00", "brand": "Everyuth", "title": "Digital Maketing - Facebook", "month_year": "Jan 2022", "number_of_files": 3},
+        {"color": "#0000FF", "brand": "Everyuth", "title": "Digital Maketing - Facebook", "month_year": "Jan 2022", "number_of_files": 1},
+        {"color": "#FF00FF", "brand": "Everyuth", "title": "Digital Maketing - Facebook", "month_year": "Jan 2022", "number_of_files": 1},
+        {"color": "#000000", "brand": "Everyuth", "title": "Digital Maketing - Facebook", "month_year": "Jan 2022", "number_of_files": 1}, 
+    ]
