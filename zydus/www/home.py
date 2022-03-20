7f9f6e2513d8ea734e1d3b913ffd9a9172378bf2
Neel Bhanushali <neel@atriina.com>
2022-03-20 08:07:55 +0000

    include file for reminder card | pretty_date_future method for reminder.due_by | using context to polate reminders on homepage

--- zydus/www/home.py
+++ zydus/www/home.py
@@ -1,4 +1,5 @@
 import frappe
+import zydus
 
 def get_context(context):
     context['trending_now_list'] = [
@@ -18,3 +19,16 @@ def get_context(context):
         {"color": "#FF00FF", "brand": "Everyuth", "title": "Digital Maketing - Facebook", "month_year": "Jan 2022", "number_of_files": 1},
         {"color": "#000000", "brand": "Everyuth", "title": "Digital Maketing - Facebook", "month_year": "Jan 2022", "number_of_files": 1}, 
     ]
+
+    context['reminders'] = [
+        {"active": True, "owner": "guest", "modified_by": "Administrator", "title": "Urgent", "description": "Send the client list", "date": "2022-03-31"},
+        {"active": True, "owner": "guest", "modified_by": "guest", "title": "Urgent", "description": "Send the client list", "date": "2022-03-30"},
+        {"active": True, "owner": "guest", "modified_by": "Administrator", "title": "Urgent", "description": "Send the client list", "date": "2022-03-21"},
+        {"active": False, "owner": "guest", "modified_by": "guest", "title": "Urgent", "description": "Send the client list", "date": "2022-03-20"},
+        {"active": True, "owner": "guest", "modified_by": "guest", "title": "Urgent", "description": "Send the client list", "date": "2022-03-31"},
+        {"active": True, "owner": "guest", "modified_by": "guest", "title": "Urgent", "description": "Send the client list", "date": "2022-03-31"},
+    ]
+
+    # due_by calculation
+    for reminder in context['reminders']:
+        reminder['due_by'] = zydus.pretty_date_future(reminder['date'])
