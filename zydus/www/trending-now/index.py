6cbafffceb169d664f7ae18c2111bfe50df52194
vagrant <vagrant@vagrant.vm>
2022-03-28 10:32:26 +0000

    Home page

--- zydus/www/trending-now/index.py
+++ zydus/www/trending-now/index.py
@@ -1,10 +1,10 @@
+
 import frappe
 import zydus
+from frappe.desk.form.load import get_attachments
 
 def get_context(context):
-    context['trending_now_list'] = frappe.db.sql(""" select B.color,B.brand_logo,P.title,concat(P.month," ",P.year) as month_year from `tabProject` as P  left join `tabBrand` as B 
-    on P.brand = B.name left join `tabView Log` as V on V.reference_name=P.name group by P.name order by count(V.reference_name) desc limit 16 """,as_dict=True)
-   
+    context['trending_now_list'] = frappe.db.sql("""select B.color,P.name,B.brand_logo,P.title,count(V.reference_name) as view,concat(P.month," ",P.year) as month_year from `tabProject` as P  left join `tabBrand` as B on P.brand = B.name left join `tabView Log` as V on V.reference_name=P.name and V.reference_doctype="Project" group by P.name order by count(V.reference_name) desc limit 16 """,as_dict=True)   
     for trending_now in context['trending_now_list']:
-        trending_now['number_of_files'] = 8
+        trending_now['number_of_files'] = len(get_attachments("Project",trending_now.name))
         trending_now["four_columns"]=True
