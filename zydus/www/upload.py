86d2a2754c750c5f8d65cfe14b9e06cd24fe0bb9
Neel Bhanushali <neel@atriina.com>
2022-03-30 11:33:47 +0000

    added tagify css and js on upload page | put restrictions on viewing upload page

---
+++ zydus/www/upload.py
@@ -0,0 +1,7 @@
+import frappe
+
+def get_context(context):
+    context['roles'] =  frappe.get_roles(frappe.session.user)
+    context['allowed_roles'] = ['KMS Uploader', 'KMS Downloader', 'KMS Admin']
+    # Sauce: https://stackoverflow.com/a/50633946/9403680
+    context['access_allowed'] = any(role in context['roles'] for role in context['allowed_roles'])
