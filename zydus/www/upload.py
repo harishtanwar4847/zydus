a92bb7a53070f4b258ce43b305bdea9d62c20d4a
Neel Bhanushali <neel@atriina.com>
2022-03-30 12:42:43 +0000

    setting up tagify inputs | populating year and brand in select inputs

--- zydus/www/upload.py
+++ zydus/www/upload.py
@@ -5,3 +5,6 @@ def get_context(context):
     context['allowed_roles'] = ['KMS Uploader', 'KMS Downloader', 'KMS Admin']
     # Sauce: https://stackoverflow.com/a/50633946/9403680
     context['access_allowed'] = any(role in context['roles'] for role in context['allowed_roles'])
+
+    if context['access_allowed']:
+        context['brands'] = [brand.name for brand in frappe.get_list('Brand')]
