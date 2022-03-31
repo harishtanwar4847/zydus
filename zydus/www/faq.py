74832bf789cd723b78ce8304602f7905e1443a8f
Neel Bhanushali <neel@atriina.com>
2022-03-31 13:57:59 +0000

    Update zydus/www/faq.py, zydus/www/faq.html

--- zydus/www/faq.py
+++ zydus/www/faq.py
@@ -9,5 +9,5 @@ def get_context(context):
     context['access_allowed'] = any(role in context['roles'] for role in context['allowed_roles'])
 
     if context['access_allowed']:
-        context['FAQs'] = frappe.db.sql("""select name,question,answers from `tabAdd FAQ`""",as_dict=1)      
+        context['faqs'] = frappe.db.sql("""select name,question,answers from `tabAdd FAQ`""",as_dict=1)      
             
