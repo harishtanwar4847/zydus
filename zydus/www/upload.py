eb4cdeba9a497c3319c9bfaef074dbf901a42d2e
Neel Bhanushali <neel@atriina.com>
2022-04-11 06:49:38 +0000

    removed validation for month and year from upload project form on submit | markets done in upload project

--- zydus/www/upload.py
+++ zydus/www/upload.py
@@ -8,10 +8,11 @@ def get_context(context):
     context['access_allowed'] = any(role in context['roles'] for role in context['allowed_roles'])
 
     if context['access_allowed']:
-        context['brands'] = [brand.name for brand in frappe.get_list('Brand')]
-        context['agencies'] = [agency.name for agency in frappe.get_list('Agency')]
-        context['project_types'] = [project_type.name for project_type in frappe.get_list('Project Type')]
-        context['data_types'] = [data_type.name for data_type in frappe.get_list('Data Type')]
+        context['brands'] = [brand.name for brand in frappe.get_all('Brand')]
+        context['agencies'] = [agency.name for agency in frappe.get_all('Agency')]
+        context['project_types'] = [project_type.name for project_type in frappe.get_all('Project Type')]
+        context['data_types'] = [data_type.name for data_type in frappe.get_all('Data Type')]
+        context['cities'] = [city.name for city in frappe.get_all('City')]
     
         context["notifications"] = frappe.db.get_all("Notification Log",fields=["subject","creation"],limit_page_length=5)
 
