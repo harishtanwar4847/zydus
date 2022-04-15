f6d60a18d22e401212f37662b0962ae66be189b4
Neel Bhanushali <neel@atriina.com>
2022-04-15 15:37:19 +0000

    download all button on home and trending now page

--- zydus/www/trending-now/index.py
+++ zydus/www/trending-now/index.py
@@ -12,5 +12,5 @@ def get_context(context):
     if context['access_allowed']:
         context['trending_now_list'] = frappe.db.sql("""select P.route,B.color,P.name,B.brand_logo,P.title,count(V.reference_name) as view,concat(P.month," ",P.year) as month_year from `tabProject` as P  left join `tabBrand` as B on P.brand = B.name left join `tabView Log` as V on V.reference_name=P.name and V.reference_doctype="Project" group by P.name having count(V.reference_name) > 0 order by count(V.reference_name) desc limit 16 """,as_dict=True)   
         for trending_now in context['trending_now_list']:
-            trending_now['number_of_files'] = len(get_attachments("Project",trending_now.name))
+            trending_now['attachments'] = get_attachments("Project",trending_now.name)
             trending_now["four_columns"]=True
