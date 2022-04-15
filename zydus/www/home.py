bdfcefd8a64857efc5caf16a131c25f96113b13f
Neel Bhanushali <neel@atriina.com>
2022-04-15 16:40:38 +0000

    fav api and integration

--- zydus/www/home.py
+++ zydus/www/home.py
@@ -10,11 +10,11 @@ def get_context(context):
     context['access_allowed'] = any(role in context['roles'] for role in context['allowed_roles'])
 
     if context['access_allowed']:
-        context['trending_now_list'] = frappe.db.sql("""select P.route,B.color,P.name,B.brand_logo,P.title,count(V.reference_name) as view,concat(P.month," ",P.year) as month_year from `tabProject` as P  left join `tabBrand` as B on P.brand = B.name left join `tabView Log` as V on V.reference_name=P.name and V.reference_doctype="Project" group by P.name having count(V.reference_name) > 0 order by count(V.reference_name) desc limit 6 """,as_dict=True)
+        context['trending_now_list'] = frappe.db.sql("""select P.route,P._liked_by as liked_by,B.color,P.name,B.brand_logo,P.title,count(V.reference_name) as view,concat(P.month," ",P.year) as month_year from `tabProject` as P  left join `tabBrand` as B on P.brand = B.name left join `tabView Log` as V on V.reference_name=P.name and V.reference_doctype="Project" group by P.name having count(V.reference_name) > 0 order by count(V.reference_name) desc limit 6 """,as_dict=True)
         for trending_now in context['trending_now_list']:
             trending_now['attachments'] = get_attachments("Project",trending_now.name)
 
-        context['my_uploads'] = frappe.db.sql(""" select P.name,route,title,color,brand,concat(month," ",year)as month_year from `tabProject` P left join `tabBrand` on P.brand = tabBrand.name group by P.name  limit 10 """,as_dict=1)
+        context['my_uploads'] = frappe.db.sql(""" select P.name,P._liked_by as liked_by,route,title,color,brand,concat(month," ",year)as month_year from `tabProject` P left join `tabBrand` on P.brand = tabBrand.name group by P.name  limit 10 """,as_dict=1)
         for my_upload in context['my_uploads']:
             my_upload['attachments'] = get_attachments("Project",my_upload.name)
         
