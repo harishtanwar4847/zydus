86c2ad6aa9940bb536cd329f22cb84544d49710f
Neel Bhanushali <neel@atriina.com>
2022-04-15 15:04:23 +0000

    view project link added on my uploads and trending now cards

--- zydus/www/home.py
+++ zydus/www/home.py
@@ -10,11 +10,11 @@ def get_context(context):
     context['access_allowed'] = any(role in context['roles'] for role in context['allowed_roles'])
 
     if context['access_allowed']:
-        context['trending_now_list'] = frappe.db.sql("""select B.color,P.name,B.brand_logo,P.title,count(V.reference_name) as view,concat(P.month," ",P.year) as month_year from `tabProject` as P  left join `tabBrand` as B on P.brand = B.name left join `tabView Log` as V on V.reference_name=P.name and V.reference_doctype="Project" group by P.name having count(V.reference_name) > 0 order by count(V.reference_name) desc limit 6 """,as_dict=True)
+        context['trending_now_list'] = frappe.db.sql("""select P.route,B.color,P.name,B.brand_logo,P.title,count(V.reference_name) as view,concat(P.month," ",P.year) as month_year from `tabProject` as P  left join `tabBrand` as B on P.brand = B.name left join `tabView Log` as V on V.reference_name=P.name and V.reference_doctype="Project" group by P.name having count(V.reference_name) > 0 order by count(V.reference_name) desc limit 6 """,as_dict=True)
         for trending_now in context['trending_now_list']:
             trending_now['number_of_files'] = len(get_attachments("Project",trending_now.name))
 
-        context['my_uploads'] = frappe.db.sql(""" select title,color,brand,count(file_name) as number_of_files,concat(month," ",year)as month_year from `tabProject` left join `tabBrand` on tabProject.brand = tabBrand.name left join `tabFile` on `tabFile`.attached_to_name = `tabProject`.name and `tabFile`.attached_to_doctype = "Project" group by `tabProject`.name  limit 10 """,as_dict=1)
+        context['my_uploads'] = frappe.db.sql(""" select route,title,color,brand,count(file_name) as number_of_files,concat(month," ",year)as month_year from `tabProject` left join `tabBrand` on tabProject.brand = tabBrand.name left join `tabFile` on `tabFile`.attached_to_name = `tabProject`.name and `tabFile`.attached_to_doctype = "Project" group by `tabProject`.name  limit 10 """,as_dict=1)
         
         context["reminders"]=frappe.db.get_list("ToDo",fields=["title","description","owner","modified_by","date"],debug=1,filters={"owner":frappe.session.user,"status":"open"},limit_page_length=5)
         
