5d43d556b8bb1f445ab2c90181d2dae170f8f062
Neel Bhanushali <neel@atriina.com>
2022-04-21 14:47:04 +0000

    datasheet query added

--- zydus/www/home.py
+++ zydus/www/home.py
@@ -12,20 +12,20 @@ def get_context(context):
 
     if context['access_allowed']:
         context['trending_now_list'] = frappe.db.sql("""select T.doctype,T.route,T.liked_by,T.name,T.title,T.month_year,B.color,B.brand_logo,count(V.reference_name) as view_count from 
-        (select "Project" as doctype, P.route,P._liked_by as liked_by,P.brand,P.name,P.title,concat(P.month," ",P.year) as month_year from `tabProject` as P 
+        (select docstatus, "Project" as doctype, P.route,P._liked_by as liked_by,P.brand,P.name,P.title,concat(P.month," ",P.year) as month_year from `tabProject` as P 
         union
-        select "Datasheet" as doctype, D.data_type, D._liked_by as liked_by,D.brand,D.name,D.title,concat(D.month," ",D.year) as month_year from `tabDatasheet`as D) as T left join `tabBrand` as B on T.brand = B.name left join `tabView Log` as V on V.reference_name= T.name and V.reference_doctype= T.doctype group by T.name having view_count > 0 order by view_count desc limit 6 """,as_dict=True)
+        select docstatus, "Datasheet" as doctype, D.data_type, D._liked_by as liked_by,D.brand,D.name,D.title,concat(D.month," ",D.year) as month_year from `tabDatasheet`as D) as T left join `tabBrand` as B on T.brand = B.name left join `tabView Log` as V on V.reference_name= T.name and V.reference_doctype= T.doctype and T.docstatus = 1 group by T.name having view_count > 0 order by view_count desc limit 6 """,as_dict=True)
         for trending_now in context['trending_now_list']:
-            trending_now['attachments'] = get_attachments("Project",trending_now.name)
-            trending_now['liked_by'] = ','.join(json.loads(trending_now['liked_by']))
+            trending_now['attachments'] = get_attachments(trending_now.doctype,trending_now.name)
+            trending_now['liked_by'] = ','.join(json.loads(trending_now['liked_by'] or '[]'))
 
-        context['my_uploads'] = frappe.db.sql(""" select T.doctype,T.route,T.liked_by,T.name,T.title,T.month_year,B.color,B.brand_name from 
-        (select "Project" as doctype, P.route,P._liked_by as liked_by,P.brand,P.name,P.title,concat(P.month," ",P.year) as month_year from `tabProject` as P 
+        context['my_uploads'] = frappe.db.sql(""" select T.doctype,T.route,T.liked_by,T.name,T.title,T.month_year,B.color,T.brand from 
+        (select docstatus, owner, "Project" as doctype, P.route,P._liked_by as liked_by,P.brand,P.name,P.title,concat(P.month," ",P.year) as month_year from `tabProject` as P 
         union
-        select "Datasheet" as doctype, D.data_type, D._liked_by as liked_by,D.brand,D.name,D.title,concat(D.month," ",D.year) as month_year from `tabDatasheet`as D) as T left join `tabBrand` as B on T.brand = B.name group by T.name  limit 10 """,as_dict=1)
+        select docstatus, owner, "Datasheet" as doctype, D.data_type, D._liked_by as liked_by,D.brand,D.name,D.title,concat(D.month," ",D.year) as month_year from `tabDatasheet`as D) as T left join `tabBrand` as B on T.brand = B.name and T.docstatus = 1 and T.owner = %s group by T.name  limit 10 """, (frappe.session.user),as_dict=1)
         for my_upload in context['my_uploads']:
-            my_upload['attachments'] = get_attachments("Project",my_upload.name)
-            my_upload['liked_by'] = ','.join(json.loads(my_upload['liked_by']))
+            my_upload['attachments'] = get_attachments(my_upload.doctype,my_upload.name)
+            my_upload['liked_by'] = ','.join(json.loads(my_upload['liked_by'] or '[]'))
         
         context["reminders"]=frappe.db.get_list("ToDo",fields=["title","description","owner","modified_by","date"],debug=1,filters={"owner":frappe.session.user,"status":"open"},limit_page_length=5)
         
