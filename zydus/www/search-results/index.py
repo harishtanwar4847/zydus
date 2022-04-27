24be9dbd378ad2476e708c288853eda4bdf1998a
Neel Bhanushali <neel@atriina.com>
2022-04-27 04:57:00 +0000

    fav button handling

--- zydus/www/search-results/index.py
+++ zydus/www/search-results/index.py
@@ -35,6 +35,6 @@ def get_context(context):
 
         for search_result in context['search_results']:
             search_result['attachments'] = get_attachments('Project', search_result.name)
-            search_result['liked_by'] = ','.join(json.loads(search_result['liked_by'] or '[]'))
+            search_result['is_liked'] = frappe.session.user in json.loads(search_result['liked_by'] or '')
             
                   
