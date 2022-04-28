8cc0b1d3e487ad3e6b7a0406b93cfe2fbf2a662b
Neel Bhanushali <neel@atriina.com>
2022-04-28 15:00:47 +0000

    tags fixed on upload

--- zydus/public/js/zyduswellness.js
+++ zydus/public/js/zyduswellness.js
@@ -1,38 +1,3 @@
-// jQuery(function () {
-//   jQuery(".drop-zone").on("click", function () {
-//     $(this).addClass("d-none");
-//     $(".drop-zone-file-list").removeClass("d-none");
-//   });
-// });
-// document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
-//   const dropZoneElement = inputElement.closest(".drop-zone");
-//   dropZoneElement.addEventListener("click", (e) => {
-//     inputElement.click();
-//   });
-  // inputElement.addEventListener("change", (e) => {
-  //   if (inputElement.files.length) {
-  //     updateThumbnail(dropZoneElement, inputElement.files[0]);
-  //   }
-  // });
-  dropZoneElement.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    dropZoneElement.classList.add("drop-zone--over");
-  });
-  ["dragleave", "dragend"].forEach((type) => {
-    dropZoneElement.addEventListener(type, (e) => {
-      dropZoneElement.classList.remove("drop-zone--over");
-    });
-  });
-  dropZoneElement.addEventListener("drop", (e) => {
-    e.preventDefault();
-    if (e.dataTransfer.files.length) {
-      inputElement.files = e.dataTransfer.files;
-      updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-    }
-    dropZoneElement.classList.remove("drop-zone--over");
-  });
-});
-
 window.onload = function () {
   //Initialize Tag Input
   var input = document.querySelectorAll("input[data-type=tags]");
