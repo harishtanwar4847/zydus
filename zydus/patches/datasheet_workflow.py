9a8af78eef16294b1883e2afd94764df8745e0ed
manjunath.raikar <manjunath.raikar@atriina.com>
2022-06-06 05:29:13 +0000

    patches-imported

---
+++ zydus/patches/datasheet_workflow.py
@@ -0,0 +1,6 @@
+import frappe
+from frappe.core.doctype.data_import.data_import import import_file
+
+def execute():
+    path = frappe.get_app_path('zydus','patches','imports','datasheet_workflow.csv')
+    import_file('Workflow', path, 'Insert',console=True)
