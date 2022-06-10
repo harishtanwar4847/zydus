7f96fe5d729113797a3154a2db6a64389a1a392a
Ankit Patil <ankit.patil@atriina.com>
2022-06-10 07:07:44 +0000

    uat issues fixed-completed

--- zydus/www/trending-now/index.py
+++ zydus/www/trending-now/index.py
@@ -5,6 +5,7 @@ from frappe.desk.form.load import get_attachments
 import json
 
 def get_context(context):
+    context['no_cache'] = 1
     context['roles'] =  frappe.get_roles(frappe.session.user)
     context['allowed_roles'] = ['KMS Uploader', 'KMS Downloader', 'KMS Admin']
     # Sauce: https://stackoverflow.com/a/50633946/9403680
@@ -12,9 +13,9 @@ def get_context(context):
 
     if context['access_allowed']:
         context['trending_now_list'] = frappe.db.sql("""select T.doctype,T.route,T.liked_by,T.name,T.title,T.month_year,B.color,B.brand_logo,count(V.reference_name) as view_count from 
-        (select docstatus, "Project" as doctype, P.route,P._liked_by as liked_by,P.brand,P.name,P.title,concat(P.month," ",P.year) as month_year from `tabProject` as P 
+        (select docstatus, "Project" as doctype, P.route,P._liked_by as liked_by,P.brand,P.name,P.p_title as title,concat(P.month," ",P.year) as month_year from `tabProject` as P 
         union
-        select docstatus, "Datasheet" as doctype, D.route, D._liked_by as liked_by,D.brand,D.name,D.title,concat(D.month," ",D.year) as month_year from `tabDatasheet`as D) as T left join `tabBrand` as B on T.brand = B.name left join `tabView Log` as V on V.reference_name= T.name and V.reference_doctype= T.doctype and T.docstatus = 1 group by T.name having view_count > 0 order by view_count desc limit 16 """,as_dict=True)   
+        select docstatus, "Datasheet" as doctype, D.route, D._liked_by as liked_by,D.brand,D.name,D.d_title as title,concat(D.month," ",D.year) as month_year from `tabDatasheet`as D) as T left join `tabBrand` as B on T.brand = B.name left join `tabView Log` as V on V.reference_name= T.name and V.reference_doctype= T.doctype and T.docstatus = 1 group by T.name having view_count > 0 order by view_count desc limit 16 """,as_dict=True)   
         for trending_now in context['trending_now_list']:
             trending_now['attachments'] = get_attachments(trending_now.doctype, trending_now.name)
             trending_now['is_liked'] = frappe.session.user in json.loads(trending_now['liked_by'] or '[]')
