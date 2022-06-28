863e179a6478172b02994a5ce575ce78d862df4a
Ankit Patil <ankit.patil@atriina.com>
2022-06-28 12:12:13 +0000

    home page changes

--- zydus/www/home.py
+++ zydus/www/home.py
@@ -12,10 +12,10 @@ def get_context(context):
     context['access_allowed'] = any(role in context['roles'] for role in context['allowed_roles'])
 
     if context['access_allowed']:
-        context['trending_now_list'] = frappe.db.sql("""select T.doctype,T.route,T.liked_by,T.name,T.title,T.month_year,B.color,B.brand_logo,count(V.reference_name) as view_count from 
-        (select docstatus, "Project" as doctype, P.route,P._liked_by as liked_by,P.brand,P.name,P.p_title as title,concat(P.month," ",P.year) as month_year from `tabProject` as P 
+        context['trending_now_list'] = frappe.db.sql("""select T.owner,T.doctype,T.route,T.liked_by,T.name,T.title,T.month_year,T.workflow_state,B.  color,B.brand_logo,U.full_name,U.user_image,count(V.reference_name) as view_count from 
+        (select docstatus, "Project" as doctype,P.owner,P.route,P._liked_by as liked_by,P.brand,P.name,P.p_title as title,P.workflow_state,concat(P.month," ",P.year) as month_year from `tabProject` as P 
         union
-        select docstatus, "Datasheet" as doctype, D.route, D._liked_by as liked_by,D.brand,D.name,D.d_title as title,concat(D.month," ",D.year) as month_year from `tabDatasheet`as D) as T left join `tabBrand` as B on T.brand = B.name left join `tabView Log` as V on V.reference_name= T.name and V.reference_doctype= T.doctype and T.docstatus = 1 group by T.name having view_count > 0 order by view_count desc limit 6 """,as_dict=True)
+        select docstatus, "Datasheet" as doctype,D.owner, D.route, D._liked_by as liked_by,D.brand,D.name,D.d_title as title,D.workflow_state,concat(D.month," ",D.year) as month_year from `tabDatasheet`as D) as T left join `tabBrand` as B on T.brand = B.name left join `tabUser`as U on U.name = T.owner left join `tabView Log` as V on V.reference_name= T.name and V.reference_doctype= T.doctype and T.docstatus = 1 group by T.name having view_count > 0 order by view_count desc limit 6 """,as_dict=True,debug=1)
         for trending_now in context['trending_now_list']:
             trending_now['attachments'] = get_attachments(trending_now.doctype,trending_now.name)
             trending_now['is_liked'] = frappe.session.user in json.loads(trending_now['liked_by'] or '[]')
