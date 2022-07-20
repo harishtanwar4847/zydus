03d97649760254e685990148c3ab64a1dd4846b3
manjunath.raikar <manjunath.raikar@atriina.com>
2022-07-20 11:47:17 +0000

    maxfile size 100mb

---
+++ zydus/patches/max_file.py
@@ -0,0 +1,12 @@
+import frappe
+
+
+def execute():
+    site_name = frappe.utils.get_site_base_path()
+    site_name = site_name.replace("./", "")
+
+    frappe.utils.execute_in_shell(
+        "bench --site {site_name} set-config max_file_size 104857600".format(
+            site_name=site_name
+        )
+    )
