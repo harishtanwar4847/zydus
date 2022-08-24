fd89d589084b5809366a81817bf46abc25896571
Manjunath Raikar <manjunath.raikar@atriina.com>
2022-08-24 11:05:30 +0000

    Update zydus/__init__.py

--- zydus/__init__.py
+++ zydus/__init__.py
@@ -3,7 +3,7 @@ import datetime
 from frappe.utils.data import DATE_FORMAT, nowdate
 import frappe
 
-__version__ = '1.2.6-uat'
+__version__ = '1.2.7-dev'
 
 # Sauce: frappe.utils.data.pretty_date
 def pretty_date_future(iso_datetime):
