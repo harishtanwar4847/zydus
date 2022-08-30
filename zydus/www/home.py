60a75cd5006443726e508212fd96fe6604fa8b48
manjunath.raikar <manjunath.raikar@atriina.com>
2022-08-30 09:41:42 +0000

    home page

--- zydus/www/home.py
+++ zydus/www/home.py
@@ -45,6 +45,9 @@ def get_context(context):
         (select docstatus, "Project" as doctype,P.owner,P.route,P._liked_by as liked_by,P.brand,P.name,P.p_title as title,P.workflow_state,concat(P.month," ",P.year) as month_year from `tabProject` as P 
         union
         select docstatus, "Datasheet" as doctype,D.owner, D.route, D._liked_by as liked_by,D.brand,D.name,D.d_title as title,D.workflow_state,concat(D.month," ",D.year) as month_year from `tabDatasheet`as D) as T left join `tabBrand` as B on T.brand = B.name left join `tabUser`as U on U.name = T.owner left join `tabView Log` as V on V.reference_name= T.name and V.reference_doctype= T.doctype and T.workflow_state = "pending" group by T.name having view_count > 0 order by view_count desc limit 6 """,as_dict=True,debug=1)
+        for trending_now in context['trending_now_list']:
+            trending_now['attachments'] = get_attachments(trending_now.doctype,trending_now.name)
+            trending_now['is_liked'] = frappe.session.user in json.loads(trending_now['liked_by'] or '[]')
         
         context['my_uploads'] = frappe.db.sql("""  select T.modified,T.doctype,T.owner,T.route,T.liked_by,T.name,T.title,T.month_year,B.color,T.brand from (select modified,docstatus, owner, "Project" as doctype, P.route,P._liked_by as liked_by,P.brand,P.name,P.p_title as title,concat(P.month," ",P.year) as month_year from `tabProject` as P where owner= %s and P.docstatus = 1        union         select modified,docstatus, owner, "Datasheet" as doctype, D.route, D._liked_by as liked_by,D.brand,D.name,D.d_title as title,concat(D.month," ",D.year) as month_year from `tabDatasheet`as D where owner = %s and D.docstatus = 1) as T left join `tabBrand` as B on T.brand = B.name order by modified desc limit 10 """, (frappe.session.user,frappe.session.user),as_dict=1,debug=1)
         for my_upload in context['my_uploads']:
