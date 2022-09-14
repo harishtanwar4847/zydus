170d9037db44122fe1c757982e4dca86d15bbc88
manjunath.raikar <manjunath.raikar@atriina.com>
2022-09-14 06:24:00 +0000

    editproject.py done

--- zydus/www/editproject.py
+++ zydus/www/editproject.py
@@ -11,7 +11,7 @@ def get_context(context):
     context['admin_access_allowed'] = any(role in context['roles'] for role in context['admin_allowed_roles'])
 
 
-    if context['admin_access_allowed']:
+    if context['admin_access_allowed'] or (context["user_access_allowed"]):
         context.doc=frappe.get_doc("Project",frappe.form_dict.edit)
         context.user_tag=str(context.doc._user_tags).split(",")[1:]
         context.market=[i.city for i in context.doc.markets]
@@ -33,26 +33,6 @@ def get_context(context):
 
         for notification in context['notifications']:
             notification['creations'] = pretty_date(notification['creation'])
-    elif context['user_access_allowed']:
-        context.doc=frappe.get_doc("Project",frappe.form_dict.edit)
-        context.user_tag=str(context.doc._user_tags).split(",")[1:]
-        context.market=[i.city for i in context.doc.markets]
-        context.color=frappe.get_value("Brand",{"name":context.doc.brand},"color")
-        context.username=frappe.get_value("User",{"email":context.doc.owner},"full_name")
-        context.image=frappe.get_value("User",{"email":context.doc.owner},"user_image")
-        context['attachments'] = frappe.desk.form.load.get_attachments('Project', frappe.form_dict.edit)
-        for attachment in context['attachments']:
-            file_ext=attachment['file_name']
-            attachment['ext']=file_ext.rsplit('.', 1)[1]
-
-        context['brands'] = [brand.name for brand in frappe.get_all('Brand')]
-        context['agencies'] = [agency.name for agency in frappe.get_all('Agency')]
-        context['project_types'] = [project_type.name for project_type in frappe.get_all('Project Type')]
-        context['data_types'] = [data_type.name for data_type in frappe.get_all('Data Type')]
-        context['cities'] = [city.name for city in frappe.get_all('City')]
-        context["edit_projects"]=frappe.db.get_list("Project",fields=["name","p_title","brand"],filters={"owner":frappe.session.user})
-        context["notifications"] = frappe.db.get_all("Notification Log",fields=["subject","creation"], filters={'for_user': frappe.session.user}, limit_page_length=5)
-        for notification in context['notifications']:
-            notification['creations'] = pretty_date(notification['creation'])
+   
         
     
