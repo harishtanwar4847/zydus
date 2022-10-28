fd6557cca3358bb0ff9d2bdc0dd03b7c23f8b023
manjunath.raikar <manjunath.raikar@atriina.com>
2022-10-28 07:18:01 +0000

    view project comment box functionality added

--- zydus/zydus/doctype/project/project.py
+++ zydus/zydus/doctype/project/project.py
@@ -30,6 +30,11 @@ class Project(WebsiteGenerator):
 		context["notifications"] = frappe.db.get_all("Notification Log",fields=["subject","creation"], filters={'for_user': frappe.session.user}, limit_page_length=5,order_by="modified desc")
 		for notification in context['notifications']:
 			notification['creations'] = pretty_date(notification['creation'])
+		context.userfullname =frappe.db.get_value("User",frappe.session.user,"full_name")
+		context["all_comments"]=frappe.db.sql(""" select C.content,C.reference_name,C.reference_doctype,C.comment_by,C.creation from `tabComment` as C left join `tabProject`  as P on reference_name = P.name where C.reference_name = %s and C.content != "" and C.comment_type="Comment" order by C.creation desc limit 10""",(context.doc.name),as_dict=True)
+		for comment in context['all_comments']:
+			comment['creations'] = pretty_date(comment['creation'])
+
 
 	def before_submit(self):
 		frappe.db.delete("View Log",{"reference_doctype": "Project", "reference_name":self.name})
